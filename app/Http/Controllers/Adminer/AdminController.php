3fb0aeaa7a38ae6f6af28d44a0a17fdf4fdf2def
albsbz <best.shop.bot@gmail.com>
2020-01-18 19:33:21 +0200

    https2

--- app/Http/Controllers/Adminer/AdminController.php
+++ app/Http/Controllers/Adminer/AdminController.php
@@ -12,21 +12,21 @@ class AdminController extends Controller
     public function index(){
 
 
-        $directories = Storage::directories('public/photos/shares/products/');
-        foreach ($directories as $directory) { 
-            $directory = implode('/', array_slice(explode ('/',$directory), 1));
-            $name=str_random(5);
-            $mainPath=asset('storage/public/'.$directory.'/');
-            $mainImg=asset('storage/public/'.implode('/', array_slice(explode ('/',Storage::files('public/'.$directory.'/')['0']), 1)));
-            $gallery=Storage::files('public/'.$directory.'/gallery');
-            foreach($gallery as $key=>$galImg){
-                $gallery[$key]=asset('storage/public/'.implode('/', array_slice(explode ('/',$galImg), 1)));
-            }
+        // $directories = Storage::directories('public/photos/shares/products/');
+        // foreach ($directories as $directory) { 
+        //     $directory = implode('/', array_slice(explode ('/',$directory), 1));
+        //     $name=str_random(5);
+        //     $mainPath=asset('storage/public/'.$directory.'/');
+        //     $mainImg=asset('storage/public/'.implode('/', array_slice(explode ('/',Storage::files('public/'.$directory.'/')['0']), 1)));
+        //     $gallery=Storage::files('public/'.$directory.'/gallery');
+        //     foreach($gallery as $key=>$galImg){
+        //         $gallery[$key]=asset('storage/public/'.implode('/', array_slice(explode ('/',$galImg), 1)));
+        //     }
            
-            $galImgs=implode(', ', $gallery);
+        //     $galImgs=implode(', ', $gallery);
            
-        }
-         return view('admin2.index', compact('directories','name','mainPath','mainImg','gallery','galImgs'));
+        // }
+         return view('admin2.index');
     }
 }
 //try
